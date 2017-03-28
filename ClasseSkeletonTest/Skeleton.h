4624bf1b48f4f3f8e38a32b5d7e8c6e974e27c15
LasceteSohei <hadrien195@hotmail.fr>
2017-03-28 10:56:37 +0200

    Renaming classes

---
+++ ClasseSkeletonTest/Skeleton.h
@@ -0,0 +1,52 @@
+#ifndef DEF_SKELETON
+#define DEF_SKELETON
+
+
+#include "includes.h"
+#include "Bone.h"
+
+class Skeleton
+{
+
+	public:
+		/************CONSTRUCTION***********/
+	Skeleton();
+
+		/************METHODES************/
+
+
+
+
+	
+		/************DESTRUCTEUR***********/
+	~Skeleton();
+
+	
+		/************FRIENDS**************/
+	
+
+
+	private:
+
+// Le nom du squelette (subject chez Vicon) : 
+	std::string m_nomSkeleton;
+
+// Le nom du bone 'root' (segment 'root' chez Vicon)
+	std::string m_nomRootBone;
+
+// Le nbre de bones du squelette :
+	unsigned int m_bonesCount;
+
+// L'ensemble des bones du squelette :
+	std::vector<Bone>m_allBones;
+};
+
+
+
+
+
+
+
+
+
+#endif
